dc9a5a271bc345ffc22fffff2dc2f395da6f1c09
sallah26 <143meski@gmail.com>
2024-03-11 11:18:41 +0300

    auth backend tackeld

---
+++ server/src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv/config";
+import { userRouter } from "./router/users.js";
+const PORT = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/auth", userRouter);
+
+
+
+
+mongoose.connect(DB_URL).then(()=>{
+    console.log( "MongoDB Connected Successfully");
+}).catch((err)=>console.error("error with ur db sallah " + err));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});
+
---
+++ server/src/router/users.js
@@ -0,0 +1,66 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../model/user.js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res)=>{
+    const {username, password} = req.body;
+    
+    try {
+        const { username, password } = req.body;
+        const existingUser = await UserModel.findOne({ username });
+        if (existingUser) {
+            return res.status(409).send("Username already exists");
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log("registerd hashedPassword = " + hashedPassword);
+        const newUser = new UserModel({
+            username,
+            password: hashedPassword,
+        });
+        await newUser.save();
+        res.status(201).send("User registered successfully");
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+router.post("/login", async (req, res)=>{
+    const {username, password} = req.body;
+    
+    try {
+        const { username, password } = req.body;
+        const existingUser = await UserModel.findOne({ username });
+        if (!existingUser) {
+            return res.status(409).send("Username is Not Found!");
+        }
+        
+        const user = req.body.username;
+        const pass = req.body.password;
+        console.log(pass);
+        const isPasswordValid = await bcrypt.compare(pass, existingUser.password) 
+        console.log(isPasswordValid);
+        if (user !== existingUser.username || !isPasswordValid){
+            return res.status(403).send('Wrong Credentials')
+        }
+        // generate a token and set it to the
+        const token = jwt.sign({id: existingUser._id}, "secret");
+        return res.status(201).send({
+            token: token,
+            message: "User Login successfully",});
+
+        
+        
+
+        // const username = req.body.username;
+        // const password = req.body.password;
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+})
+
+export { router as userRouter };
